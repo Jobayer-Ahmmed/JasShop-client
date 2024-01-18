949048b4e5105017647de2b5be4b62f75eb63b79
%Sa2323ma <jobayer.ahmmed.jas@gmail.com>
2024-01-19 01:34:59 +0600

    edit: secondary_banner

--- src/Pages/Home/SecondaryBanner/SecondaryBanner.jsx
+++ src/Pages/Home/SecondaryBanner/SecondaryBanner.jsx
@@ -1,66 +1,29 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import { Pagination, Navigation } from "swiper/modules";
-import "swiper/css/navigation";
-
-import a from "../../../assets/a.jpg";
+import fashion from "../../../assets/category/fashion.PNG";
+import beauty from "../../../assets/category/beauty.PNG";
+import electronics from "../../../assets/category/electronics.PNG";
 import { NavLink } from "react-router-dom";
-import fashion from "../../../assets/category/fashion.PNG"
-import beauty from "../../../assets/category/beauty.PNG"
-import electronics from "../../../assets/category/electronics.PNG"
-import furniture from "../../../assets/category/furniture.PNG"
 
-
-const secondaryBanner = () => {
-
+const SecondaryBanner = () => {
   return (
-    <div className="px-[10%] lg:px-20 my-xPadding secondaryMenu">
-      <div className="">
-        <Swiper
-          slidesPerView={4} // akta page a koi ta slide/ image dekha be
-                                    // spaceBetween={2}
-          navigation={true}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[Pagination, Navigation]}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <NavLink className="" to="/fashion" >
-              <img src={fashion} alt="" className="w-32 h-28" />
-              <p>Fashion</p>
-            </NavLink>
-          </SwiperSlide>
-          <SwiperSlide>
-            <NavLink to="/beauty">
-              <img src={beauty} alt="" className="w-32 h-28"  />
-              <p>Beauty</p>
-            </NavLink>
-          </SwiperSlide>
-          <SwiperSlide>
-            <NavLink to="/electronics">
-              <img src={electronics} alt="" className="w-32 h-28"  />
-              <p>Electronics</p>
-            </NavLink>
-          </SwiperSlide>
-          <SwiperSlide>
-            <NavLink to="/fashion">
-              <img src={furniture} alt="" className="w-32 h-28"  />
-              <p>Furniture</p>
-            </NavLink>
-          </SwiperSlide>
-          <SwiperSlide>
-            <NavLink to="/fashion">
-              <img src={a} alt="" className="w-32 h-28"  />
-              <p>Fashion</p>
-            </NavLink>
-          </SwiperSlide>
-        </Swiper>
-      </div>
+    <div className="flex justify-center gap-3 md:gap-7 secondary-banner">
+      <NavLink className="rounded-lg border-2 border-transparent" to="/fashion">
+        <img
+          className="w-24 h-16 md:w-32 md:h-24 rounded-lg"
+          src={fashion}
+          alt=""
+        />
+      </NavLink>
+      <NavLink className="rounded-lg border-2 border-transparent" to="/beauty">
+        <img className="w-24 h-16 md:w-32 md:h-24 rounded-lg" src={beauty} alt="" />
+      </NavLink>
+      <NavLink
+        className="rounded-lg border-2 border-transparent"
+        to="/electronics"
+      >
+        <img className="w-24 h-16 md:w-32 md:h-24 rounded-lg" src={electronics} alt="" />
+      </NavLink>
     </div>
   );
 };
 
-export default secondaryBanner;
+export default SecondaryBanner;
